e991059bfdbb622826f60d0a1de104ab52de5817
ElieSol <julie.solacroup@yahoo.fr>
2019-01-31 17:33:24 +0100

    resultats annotation

--- annotation.r
+++ annotation.r
@@ -11,6 +11,7 @@ library("RDAVIDWebService")
 library(ggplot2)
 david<-DAVIDWebService(email="julie.solacroup@etu.u-bordeaux.fr", url="https://david.ncifcrf.gov/webservice/services/DAVIDWebService.DAVIDWebServiceHttpSoap12Endpoint/")
 data(demoList1)
+
 result<-addList(david, demoList1,idType="AFFYMETRIX_3PRIME_IVT_ID",listName="demoList1", listType="Gene")
 result
 setAnnotationCategories(david, c("GOTERM_BP_ALL","GOTERM_MF_ALL", "GOTERM_CC_ALL"))
